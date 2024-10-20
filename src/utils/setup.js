dac9ccb021adad1e0b3b1cd41556b55c79c15dfe
Thuong To <thuongthanhto@gmail.com>
2024-10-20 13:25:41 +0700

    Update setup.js

--- src/utils/setup.js
+++ src/utils/setup.js
@@ -1,3 +1,4 @@
 export const WALLETCONNECT_PROJECT_ID = import.meta.env
   .VITE_WALLETCONNECT_PROJECT_ID
 export const EXPLORER_URL = import.meta.env.VITE_EXPLORER_URL
+export const ADDRESS = import.meta.env.VITE_ADDRESS
